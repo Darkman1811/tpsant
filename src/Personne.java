43ec26d2a6ba45763189973949f82ee82cdcc11d
root <root@guestu.sn+9>
2016-10-28 07:41:57 +0000

    <mkdir>

---
+++ src/Personne.java
@@ -0,0 +1,7 @@
+public class Personne{
+
+	public static void main (String [] args){
+		System.out.println("hello from java");
+        Personnedao pdao=new Personnedao();
+	}
+}
---
+++ src/Personnedao.java
@@ -0,0 +1,29 @@
+public class Personnedao{
+	private int id;
+	private String nom;
+	private int age;
+
+	public void setId(int id){
+		this.id=id;
+	}
+
+	public int getId(){
+		return this.id;
+	}
+
+	public void setNom(String nom){
+		this.nom=nom;
+	}
+
+	public String getNom(){
+		return this.nom;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+	}
+
+	public int getAge(){
+		return this.age;
+	}
+}
